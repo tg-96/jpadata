902a4fa0bf431e3020f2af59df7ac42de9dc4789
정태규 <jtg960528@gmail.com>
2023-09-15 21:46:46 +0900

    복잡한 정적쿼리에서 사용 @Query("jpql") compile 시점에서 오류를 잡아낼 수 있어서 좋다.

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
@@ -10,5 +12,6 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
     List<Member> findTop3ByOrderByAgeAsc();
 
-    
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,7 +9,7 @@ import study.datajpa.entity.Member;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -78,11 +78,22 @@ class MemberRepositoryTest {
             System.out.println(member);
         }
 
-//        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("memberA", 15);
+        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("memberA", 15);
 
-//        assertThat(members.get(0).getUsername()).isEqualTo("memberA");
-//        assertThat(members.get(0).getAge()).isEqualTo(20);
-//        assertThat(members.size()).isEqualTo(1);
+        assertThat(members.get(0).getUsername()).isEqualTo("memberA");
+        assertThat(members.get(0).getAge()).isEqualTo(20);
+        assertThat(members.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA",10);
+        assertThat(result.get(0)).isEqualTo(m1);
     }
 
 }
